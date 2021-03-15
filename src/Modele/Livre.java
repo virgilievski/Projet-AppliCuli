861c71d17b88695448902a5d304b7e4394936f1c
LilBabines <augusteverdier@orange.fr>
2021-03-15 23:20:33 +0100

    avance Livre

--- src/Modele/Livre.java
+++ src/Modele/Livre.java
@@ -1,5 +1,15 @@
 package Modele;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Livre {
+	public HashMap<String,Recette> list_recettes;
+	public int nb_recette;
 	
+	
+	public Livre() {
+		this.list_recettes= new HashMap();
+		this.nb_recette=0;
+	}
 }
