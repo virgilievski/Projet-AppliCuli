f0c7e14a61ba827f2950939e562e9833aad17581
LilBabines <augusteverdier@orange.fr>
2021-03-15 23:06:13 +0100

    avancement claas Recette

--- src/Modele/Recette.java
+++ src/Modele/Recette.java
@@ -1,5 +1,33 @@
 package Modele;
 
-public class Recette {
+import java.util.ArrayList;
 
+public class Recette {
+	public ArrayList<Ingredient> list_ingredients;
+	public int nb_etapes;
+	public ArrayList<Etape> list_etapes;
+	public String nom;
+	
+	
+	public Recette(String nom) {
+		this.nom=nom;
+		this.list_ingredients = new ArrayList();
+		this.list_etapes=new ArrayList();
+		this.nb_etapes=0;
+		this.list_ingredients=new ArrayList();
+		
+				
+	}
+	
+	public void Ajout_ingredient(Ingredient ing) {
+		this.list_ingredients.add(ing);
+		
+	}
+	public void Ajout_etapes( Etape e) {
+		this.list_etapes.add(e);
+		this.nb_etapes+=1;
+		
+	}
+	
+	
 }
