8f7378a7f645dc01b6d81247d7940e79018a6c1d
LilBabines <augusteverdier@orange.fr>
2021-03-15 19:47:39 +0100

    ajout : début classe vu, début javaFx

---
+++ src/Vu/Appli_Stage.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package Vu;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Verdier
+ *
+ */
+public class Appli_Stage extends Application  {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		arg0.show();
+		
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
