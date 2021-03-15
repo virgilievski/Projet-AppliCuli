bbbfd824cf946bcc34d65da8dbfb5d49264a0810
ilan55 <ilan.fijalkow@icloud.com>
2021-03-15 23:20:33 +0100

    ajout de la classe étape

--- src/Modele/Etape.java
+++ src/Modele/Etape.java
@@ -1,5 +1,15 @@
 package Modele;
 
 public class Etape {
+	public int numéro;
+	public String instructions;
+	public Ingredient ingrédients;
+	
+	public Etape(int n, String i, Ingredient in) {
+		this.numéro=n;
+		this.instructions=i;
+		this.ingrédients=in;
+	}
+	
 
 }
