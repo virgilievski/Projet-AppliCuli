69c7d279fe41cf8089e070eb2e6e33edb251c0cb
virgilo <virgilo@kali>
2021-03-18 11:04:42 +0100

    bonjour

---
+++ src/Vu/Page.java
@@ -0,0 +1,14 @@
+package Vu;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Page extends Scene{
+
+	public Page(Parent arg0, double arg1, double arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
