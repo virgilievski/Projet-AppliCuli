6b50693bb2a47c6dc85aa912aa1caef26614a789
virgilo <virgilo@kali>
2021-03-29 17:46:10 +0200

    modif du parsing dans la classe livre

--- src/Modele/Recette.java
+++ src/Modele/Recette.java
@@ -43,29 +43,5 @@ public class Recette {
 		
 	}
 
-	public void parsingmaison(){
-		try
-		{
-		// Le fichier d'entrée
-		File file = new File("Recettes/choucrouteMelba.txt");    
-		// Créer l'objet File Reader
-		FileReader fr = new FileReader(file);  
-		// Créer l'objet BufferedReader        
-		BufferedReader br = new BufferedReader(fr);  
-		StringBuffer sb = new StringBuffer();    
-		String line;
-		while((line = br.readLine()) != null) {
-		        // ajoute la ligne au buffer
-		        sb.append(line);      
-		        sb.append("\n");     
-		      }
-		      fr.close();    
-		      System.out.println("Contenu du fichier: ");
-		      System.out.println(sb.toString());  
-		    }
-		    catch(IOException e)
-		    {
-		      e.printStackTrace();
-		    }
-		  }
+
 }
--- src/Vu/Appli_Stage.java
+++ src/Vu/Appli_Stage.java
@@ -1,5 +1,7 @@
 package Vu;
 
+import java.io.File;
+
 import Modele.Livre;
 import Modele.Recette;
 import javafx.application.Application;
@@ -25,38 +27,9 @@ public class Appli_Stage extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		primaryStage.setWidth(800);
-        primaryStage.setHeight(600);
-        primaryStage.setTitle("Le livre de recette");
-        BorderPane layoutroot=new BorderPane();
-        Scene scene= new Scene(layoutroot,800,600);
-        primaryStage.setScene(scene);
-        primaryStage.getIcons().add(new Image("Image/icon.jpg"));
-        // Insertion de la barre de recherche
-        HBox hbox1=new HBox();
-        hbox1.setPadding(new Insets(10));
-        hbox1.setSpacing(10);
-        Label nom= new Label("Barre de recherche");
-        nom.setPadding(new Insets(5));
-        TextField recherche= new TextField();
-        Button rechercher=new Button("Rechercher");
-        hbox1.getChildren().addAll(nom,recherche,rechercher);
-        layoutroot.setTop(hbox1);
-        
-        //Insertion des images-liens vers les recettes à la une
-        VBox vbox2 = new VBox(8);
-        vbox2.getChildren().addAll(new Label("Recettes à la une"), new Button("recette suivante")); //le bouton servira à passer à la recette suivante des recette à la une. On fait tourner 4/5 recettes.
-        layoutroot.setCenter(vbox2);
-        
-        primaryStage.show();
-        
-        
-        
-        Livre livreRecette = new Livre();
-        Recette choucrouteMelba= new Recette("Choucroute Melba","Sucré Salée");
-        Recette couscous = new Recette("Couscous", "salé");
-        livreRecette.ajoutRecette(choucrouteMelba);
-        livreRecette.ajoutRecette(couscous);
+		Livre bible = new Livre();
+		File tiramisu = new File("Recettes/tiramisu.txt");
+		bible.parsingmaison(tiramisu);
 	}
 
 }
