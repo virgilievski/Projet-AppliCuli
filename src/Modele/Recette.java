918a2be2f87c675ce2104ad408da47d2d70a4c5d
LilBabines <augusteverdier@orange.fr>
2021-03-29 15:07:20 +0200

    modif reccet askip

--- src/Modele/Recette.java
+++ src/Modele/Recette.java
@@ -1,8 +1,13 @@
 package Modele;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.TreeSet;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Recette {
 	public Set<Ingredient> list_ingredients; 
@@ -19,7 +24,8 @@ public class Recette {
 		this.nom=nom;
 		this.saveur = s;
 		this.list_ingredients = new TreeSet();
-		this.list_etapes=new ArrayList();			
+		this.list_etapes=new ArrayList();
+				
 	}
 	
 	public void Ajout_ingredient(Ingredient ing) {
@@ -28,8 +34,38 @@ public class Recette {
 	}
 	public void Ajout_etapes( Etape e) {
 		this.list_etapes.add(e);
+			
+	}
 		
+	public void txtToRecette() {
+		BufferedReader lecteurAvecBuffer =null;
+		String ligne;
 		
 	}
-	
+
+	public static void main(String args[]){
+		
+		{
+		// Le fichier d'entrée
+		File file = new File("Recettes/choucrouteMelba.txt");    
+		// Créer l'objet File Reader
+		FileReader fr = new FileReader(file);  
+		// Créer l'objet BufferedReader        
+		BufferedReader br = new BufferedReader(fr);  
+		StringBuffer sb = new StringBuffer();    
+		String line;
+		while((line = br.readLine()) != null) {
+		        // ajoute la ligne au buffer
+		        sb.append(line);      
+		        sb.append("\n");     
+		      }
+		      fr.close();    
+		      System.out.println("Contenu du fichier: ");
+		      System.out.println(sb.toString());  
+		    }
+		    catch(IOException e)
+		    {
+		      e.printStackTrace();
+		    }
+		  }
 }
