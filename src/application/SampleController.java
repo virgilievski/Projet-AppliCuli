4afbd0e2c788c27ce44f4e9e2e4272d2c17f6810
LilBabines <augusteverdier@orange.fr>
2021-03-29 22:10:33 +0200

    c'est bon on arrive a ouvrir une nouvelle fenêtrre

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -8,14 +8,27 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class SampleController {
 	
-	
-	   public void ouvrirNew(ActionEvent event) throws Exception{
+	   
+	   public void ouvrirNew(ActionEvent event) throws IOException {
+	       Stage newStageRecette = new Stage();
+	       newStageRecette.initModality(Modality.APPLICATION_MODAL);
+	       AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("StageNewRecette.fxml"));
+	       //VBox root = new VBox(20);
+	       Scene bien= new Scene(root);
+	       newStageRecette.setScene(bien);
 	       
+	       newStageRecette.showAndWait();
 	    }
 	}
 
