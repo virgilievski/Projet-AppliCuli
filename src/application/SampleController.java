12c089aa2d4daddcccbcc7458777f10c29dd32d8
LilBabines <augusteverdier@orange.fr>
2021-03-30 20:31:37 +0200

    avancement controlleur

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -8,7 +8,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
@@ -18,17 +20,26 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class SampleController {
-	
+		public Button save=null;
+	   public CheckBox bien = null;
+	   public TextField indic = null;
+	   public TextArea edit =null;
+	   public void rotate() {
+		   bien.setRotate(bien.getRotate()+1);
+	   }
 	   
 	   public void ouvrirNew(ActionEvent event) throws IOException {
 	       Stage newStageRecette = new Stage();
 	       newStageRecette.initModality(Modality.APPLICATION_MODAL);
-	       AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("StageNewRecette.fxml"));
+	       AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("StageNewReccette.fxml"));
 	       //VBox root = new VBox(20);
-	       Scene bien= new Scene(root);
-	       newStageRecette.setScene(bien);
-	       
+	       Scene a= new Scene(root);
+	       newStageRecette.setScene(a);
 	       newStageRecette.showAndWait();
 	    }
+	   public void Save() {
+		   indic.setText("saveur");
+		   
+	   }
 	}
 
